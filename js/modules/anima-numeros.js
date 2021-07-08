a0d994c3aba22856adb4bfe9fe3ff67a6023fb3d
unknown <gabriel.coelhox@gmail.com>
2021-07-08 16:39:45 -0300

    adicionando área para dados e sua animação

---
+++ js/modules/anima-numeros.js
@@ -0,0 +1,35 @@
+export default function initAnimaNumeros() {
+
+    function animaNumeros() {
+        const numeros = document.querySelectorAll('[data-numero]');
+        
+        numeros.forEach((numero) => {
+
+            const total = +numero.innerText;
+            const incremento = Math.floor(total / 100); // arredonda os números
+
+            let start = 0;
+            const timer = setInterval(() => {
+                start = start + incremento;
+                numero.innerText = start;
+                if(start > total) {
+                numero.innerText = total; // se passar volta para o total
+                clearInterval(timer);
+                }
+            }, 38 * Math.random()); // Velocidade aleatória, faz com que os números cheguem ao final diferente
+        });
+    }
+        
+        function handleMutation(mutation) {
+            if(mutation[0].target.classList.contains('ativo')) {
+            observer.disconnect(); // Evita que recomece ao dar scroll
+            animaNumeros();
+            }
+        }
+        
+        const observerTarget = document.querySelector('.numeros');
+        const observer = new MutationObserver(handleMutation); // Observador 
+        
+        observer.observe(observerTarget, {attributes: true});
+}
+    
